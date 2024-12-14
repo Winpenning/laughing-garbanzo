75ea42de8081fc341b1dfb4712753ff26bc2b383
Winpenning-h <zs.phenriqueprofessional@gmail.com>
2024-12-14 20:57:17 -0300

    feat: DataAnnotations adicionadas ao modelo DTO de custumer.

--- CDJ-API/Models/CustumerDTO.cs
+++ CDJ-API/Models/CustumerDTO.cs
@@ -1,9 +1,13 @@
-using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace CDJ_API.Models;
 public class CustumerDTO{
-    public required string FirstName  { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Error MC0x201: The data is required")]
+    public string FirstName  { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Error MC0x202: The data is required")]
     public string LastName { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Error MC0x203: The data is required")]
     public string PhoneNumber {get; set;} =string.Empty;
+    [Required(ErrorMessage = "Error MC0x204: The data is required")]
     public DateOnly BirthdayDate {get; set;}
 }
