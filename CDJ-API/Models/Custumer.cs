c8ffa8c0ab60fad4657db979efcd3df9245327a3
Winpenning-h <zs.phenriqueprofessional@gmail.com>
2024-12-14 17:58:20 -0300

    feat: Modelo  de Custumer implementado.

--- CDJ-API/Models/Custumer.cs
+++ CDJ-API/Models/Custumer.cs
@@ -1,35 +1,15 @@
-using System.Text.Json;
-using System.Text.Json.Serialization;
-
 namespace CDJ_API.Models;
-
 public class Custumer{
-    [JsonInclude]
-    private int Id { get; set; }
-    [JsonInclude]
-    private string FirstName  { get; set; } = string.Empty;
-    [JsonInclude]
-    private string LastName { get; set; } = string.Empty;
-    [JsonInclude]
-    private string PhoneNumber {get; set;} =string.Empty;
-    [JsonInclude]
-    private DateOnly BirthdayDate {get; set;}
+    internal int Id { get; set; }
+    internal string FirstName  { get; set; } = string.Empty;
+    internal string LastName { get; set; } = string.Empty;
+    internal string PhoneNumber {get; set;} =string.Empty;
+    internal DateOnly BirthdayDate {get; set;}
     public Custumer(){}
-
     public Custumer(string firstName, string lastName, string phoneNumber, DateOnly birthdayDate){
         FirstName = firstName;
         LastName = lastName;
         PhoneNumber = phoneNumber;
         BirthdayDate = birthdayDate;
-    }
-
-    public Custumer GetInstance(){
-        return this;
-    }
-    public string GetJson(){
-        var options = new JsonSerializerOptions{
-            WriteIndented = true
-        };
-        return JsonSerializer.Serialize(this, options);
     }
 }
