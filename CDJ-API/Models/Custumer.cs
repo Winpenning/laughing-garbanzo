339c9c8bc57afc450ceb9028dd40988aa8fbf2e8
Winpenning-h <zs.phenriqueprofessional@gmail.com>
2024-12-14 11:43:20 -0300

    feat: Modelo de Custumer alterado

--- CDJ-API/Models/Custumer.cs
+++ CDJ-API/Models/Custumer.cs
@@ -1,12 +1,19 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace CDJ_API.Models;
 
 public class Custumer{
-    public int Id { get; set; }
+    [JsonInclude]
+    private int Id { get; set; }
+    [JsonInclude]
     private string FirstName  { get; set; } = string.Empty;
+    [JsonInclude]
     private string LastName { get; set; } = string.Empty;
+    [JsonInclude]
     private string PhoneNumber {get; set;} =string.Empty;
+    [JsonInclude]
     private DateOnly BirthdayDate {get; set;}
-
     public Custumer(){}
 
     public Custumer(string firstName, string lastName, string phoneNumber, DateOnly birthdayDate){
@@ -14,5 +21,15 @@ public class Custumer{
         LastName = lastName;
         PhoneNumber = phoneNumber;
         BirthdayDate = birthdayDate;
+    }
+
+    public Custumer GetInstance(){
+        return this;
+    }
+    public string GetJson(){
+        var options = new JsonSerializerOptions{
+            WriteIndented = true
+        };
+        return JsonSerializer.Serialize(this, options);
     }
 }
