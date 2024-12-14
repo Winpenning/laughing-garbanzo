461bf0a0a75a8cc8da6772ab3dec5f6374733a9a
Winpenning-h <zs.phenriqueprofessional@gmail.com>
2024-12-14 19:01:49 -0300

    feat: Implementação de documentação de endpoints via swagger na API e configurações para realizar a conexão com o banco de dados.

--- CDJ-API/Program.cs
+++ CDJ-API/Program.cs
@@ -1,8 +1,35 @@
+using CDJ_API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigureDatabase(builder);
+ConfigureEndpoints(builder);
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+    DocumentAppEndpoints(app);
+    
 app.MapControllers();
+
 app.Run();
+
+static void ConfigureDatabase(WebApplicationBuilder builder){
+    var connectionString = builder.Configuration.GetConnectionString("Default");
+    builder.Services.AddDbContext<AppDataContext>(options =>
+    {
+    options.UseSqlServer(connectionString);
+    });
+}
+static void ConfigureEndpoints(WebApplicationBuilder builder){
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
+static void DocumentAppEndpoints(WebApplication app){
+    app.MapSwagger();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
