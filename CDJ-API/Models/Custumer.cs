8dba56e13eac3cf57827ed9f7d0da46da2dc7edf
Winpenning-h <zs.phenriqueprofessional@gmail.com>
2024-12-14 21:52:43 -0300

    feat: Modificadores de acesso em Custumer.

--- CDJ-API/Models/Custumer.cs
+++ CDJ-API/Models/Custumer.cs
@@ -1,10 +1,10 @@
 namespace CDJ_API.Models;
 public class Custumer{
-    public int Id { get; private set; }
-    public string FirstName  { get; private set; } = string.Empty;
-    public string LastName { get; private set; } = string.Empty;
-    public string PhoneNumber {get; private set;} =string.Empty;
-    public DateOnly BirthdayDate {get; private set;}
+    public int Id { get; internal set; }
+    public string FirstName  { get; internal set; } = string.Empty;
+    public string LastName { get; internal set; } = string.Empty;
+    public string PhoneNumber {get; internal set;} =string.Empty;
+    public DateOnly BirthdayDate {get; internal set;}
     public Custumer(){}
     public Custumer(string firstName, string lastName, string phoneNumber, DateOnly birthdayDate){
         FirstName = firstName;
