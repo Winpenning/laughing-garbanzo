4326a02e5bb6eef7d9774f989171b9dba0c15b65
Winpenning-h <zs.phenriqueprofessional@gmail.com>
2024-12-14 21:01:59 -0300

    feat: Início das implementações no controlador de Custumer. Primeira versão do método de Post em Custumer.

--- CDJ-API/Controllers/CustumerController.cs
+++ CDJ-API/Controllers/CustumerController.cs
@@ -1,15 +1,35 @@
+using CDJ_API.Data;
 using CDJ_API.Models;
+using CDJ_API.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CDJ_API.Controllers;
 
-[ApiController][Route("Custumer")]
+[ApiController][Route("custumer")]
 public class CustumerController : ControllerBase{
-
-    [HttpGet][Route("")]
-    public async Task<IActionResult> GetCustumer()
+    
+    [HttpPost][Route("new")]
+    public async Task<IActionResult> PostCustumer
+    ([FromServices] AppDataContext context, [FromBody] CustumerDTO model)
     {
-        Custumer custumer = new Custumer("Ted","Cleiton","123456789",new DateOnly(1998,10,30));
-        return Ok(custumer.GetJson());
+        try{
+            Custumer custumer = new Custumer(
+                model.FirstName,
+                model.LastName,
+                model.PhoneNumber,
+                model.BirthdayDate);
+
+            await context.Custumers.AddAsync(custumer);
+            await context.SaveChangesAsync();
+            return StatusCode(200, 
+            new ResultViewModel<CustumerDTO>(model));   
+        }catch(ArgumentException){
+            return StatusCode(400, 
+            new ResultViewModel<CustumerDTO>("Error CC0x101: Bad Request."));
+        }
+        catch(Exception){
+            return StatusCode(500, 
+            new ResultViewModel<CustumerDTO>("Error CC0x102: Internal Server Error."));
+        }
     }
 }
