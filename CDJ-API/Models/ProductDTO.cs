d564bbd1dd6556290dd7d09efbef67188f6e890e
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2024-12-18 08:44:09 -0300

    feat: Modelo DTO para Produto implementado.

---
+++ CDJ-API/Models/ProductDTO.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CDJ_API.Models;
+public class ProductDTO{
+    [Required(ErrorMessage = "MP0x401: The data is required")]
+    public string Name {get; internal set;} = string.Empty;
+    [Required(ErrorMessage = "MP0x402: The data is required")]
+    public string UnitType { get; internal set; } = string.Empty;
+    [Required(ErrorMessage = "MP0x403: The data is required")]
+    public int UnitCount { get; internal set; }
+    [Required(ErrorMessage = "MP0x404: The data is required")]
+    public decimal UnitPrice { get; internal set; }
+}
